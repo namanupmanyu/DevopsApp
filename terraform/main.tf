3cec4a1b87934a09e790ceef61be158f08672b83
naman <naman303060@gmail.com>
2025-04-30 10:40:23 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test"{
+         name=var.name
+         ami=var.ami_id
+         instance_type=var.instance_type
+         key_name=aws_key_pair.generated_key.key_name
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "name"  {
+        default='my-app'
+                }
+
+variable "ami_id"  {
+         default='ami-0ce45259f491c3d4f'}
+
+variable "instance_type"  {
+        default='t2.micro'
+}
